85f973bff7d2cc6bca551433a1a1911cc6a77f9b
Alexander Andersson <alexander.andersson.9347@gmail.com>
2017-03-01 14:59:18 +0100

    added logIn, signUp modals with navbar buttons

---
+++ ArenaStars/ArenaStars/Scripts/SignIn&Up.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $(".logInBtn").on("click", function () {
+        $("#login-modal").modal("show")
+        $("#register-modal").modal("hide")
+    });
+
+    $(".registerBtn").on("click", function () {
+        $("#register-modal").modal("show")
+        $("#login-modal").modal("hide")
+    });
+
+
+
+});
