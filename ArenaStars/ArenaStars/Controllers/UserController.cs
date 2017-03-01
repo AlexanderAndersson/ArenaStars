0759883f3667c4725aa197aa18f98d09449d31f7
studerande <niclas-67@hotmail.com>
2017-03-01 16:55:26 +0100

    Removed partial view, doing jquery, ajax and json for register and login

--- ArenaStars/ArenaStars/Controllers/UserController.cs
+++ ArenaStars/ArenaStars/Controllers/UserController.cs
@@ -4,67 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace ArenaStars.Controllers
 {
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult Register()
+        public ActionResult Register(string username, string email, string password, string password2)
         {
-            bool foundFaults = false;
-            List<string> ErrorMsgList = new List<string>();
-
-            if (ModelState.IsValid)
-            {
-                string username = Request["inputUsername"];
-                string password = Request["inputPassword"];
-                string password2 = Request["inputPassword2"];
-                string email = Request["InputEmail"];
-
-                if (password2 != password)
-                {
-                    ErrorMsgList.Add("Given passwords do not match!");
-                    ViewBag.passwordNotMatch = "Given passwords do not match!";
-                    foundFaults = true;
-                }
+            List<string> ErrorMsgList = checkRegisterInputFaults(username, email, password, password2);
                 
 
-                //Kolla ifall man måste kolla efter white spaces endast.
+            //Kolla ifall man måste kolla efter white spaces endast.
 
-                if (!foundFaults)
+            if (ErrorMsgList.Count == 0)
+            {
+                using (ArenaStarsContext context = new ArenaStarsContext())
                 {
-                    using (ArenaStarsContext context = new ArenaStarsContext())
+                    User newUser = new User()
                     {
-                        User newUser = new User()
-                        {
-                            Username = username,
-                            Password = password,
-                            Email = email,
-                            SignUpDate = DateTime.Now,
-                            IsAdmin = false,
-                            IsTerminated = false,
-                            LastLoggedIn = DateTime.Now,
-                            ProfilePic = "~/Images/Profile/ProfilePicture_Default.jpg",
-                            Rank = Models.User.RankEnum.Unranked
-                        };
+                        Username = username,
+                        Password = password,
+                        Email = email,
+                        SignUpDate = DateTime.Now,
+                        IsAdmin = false,
+                        IsTerminated = false,
+                        LastLoggedIn = DateTime.Now,
+                        ProfilePic = "~/Images/Profile/ProfilePicture_Default.jpg",
+                        Rank = Models.User.RankEnum.Unranked
+                    };
 
 
-                        Session["isLoggedIn"] = true;
-                        Session["username"] = username;
+                    Session["isLoggedIn"] = true;
+                    Session["username"] = username;
 
-                        context.Users.Add(newUser);
-                        context.SaveChanges();
-                    }
+                    context.Users.Add(newUser);
+                    context.SaveChanges();
                 }
             }
 
-            if (!foundFaults)
+            if (ErrorMsgList.Count == 0)
                 return RedirectToAction("/Index", "Home");
             else
             {
-                //return Json(new { errorList = ErrorMsgList }, JsonRequestBehavior.DenyGet);
-                return View();
+                return Json(new { errorList = ErrorMsgList }, JsonRequestBehavior.DenyGet);
             }
 
         }
@@ -125,28 +109,72 @@ namespace ArenaStars.Controllers
             return RedirectToAction("/Index", "Home");
         }
 
-        private List<string> checkRegisterInputFaults()
+        private List<string> checkRegisterInputFaults(string username, string email, string password, string password2)
         {
-            /*
-             
-            [Required(ErrorMessage = "Username cannot be empty!")]
-            [StringLength(30, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 30 characters.")]
-            public string Username { get; set; }
+            List<string> errorMsgList = new List<string>();
 
-            [Required(ErrorMessage = "Password cannot be empty!")]
-            [StringLength(30, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 30.")]
-            public string Password { get; set; }
+            //Checks empty or whitespace
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                errorMsgList.Add("Username cannot be empty!");
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                errorMsgList.Add("E-mail cannot be empty!");
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                errorMsgList.Add("Password cannot be empty!");
+            }
 
-            [Required]
-            [RegularExpression(
-            @"^(?("")("".+?(?<!\\)""@)|(([0-9A-Za-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9A-Za-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9A-Za-z][-\w]*[0-9A-Za-z]*\.)+[A-Za-z0-9][\-a-z0-9]{0,22}[A-Za-z0-9]))$",
-            ErrorMessage = "Given E-mail address is not valid!")]
-            public string Email { get; set; }
-             
-             */
+            //Checks lengths
+            if (username.Length >= 3 && username.Length <= 30)
+            {
+                errorMsgList.Add("Username must be between 3 and 30 characters.");
+            }
+            if (username.Length >= 6 && username.Length <= 30)
+            {
+                errorMsgList.Add("Password must be between 6 and 30 characters.");
+            }
+
+            //Checks if passwords match
+            if (password2 != password)
+            {
+                errorMsgList.Add("Given passwords do not match!");
+            }
+
+            //Regex check
+            string pattern = @"^(?("")("".+?(?<!\\)""@)|(([0-9A-Za-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9A-Za-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9A-Za-z][-\w]*[0-9A-Za-z]*\.)+[A-Za-z0-9][\-a-z0-9]{0,22}[A-Za-z0-9]))$";
+            if (Regex.IsMatch(email, pattern))
+            {
+                errorMsgList.Add("Given email address is not valid!");
+            }
 
 
-            return null;
+            using (ArenaStarsContext context = new ArenaStarsContext())
+            {
+                var checkUserExists = from u in context.Users
+                                        where username.ToLower() == u.Username.ToLower()
+                                        select u;
+
+                var checkEmailExists = from u in context.Users
+                                       where email.ToLower() == u.Email.ToLower()
+                                       select u;
+
+                //Checks if user with same username exists
+                if (checkUserExists.Count() > 0)
+                {
+                    errorMsgList.Add("Given username is taken!");
+                }
+
+                //Checks if user with same email exists
+                if (checkEmailExists.Count() > 0)
+                {
+                    errorMsgList.Add("Given email is taken!");
+                }
+            }
+
+                return errorMsgList;
         }
     }
 }
--- ArenaStars/ArenaStars/Scripts/SignIn&Up.js
+++ ArenaStars/ArenaStars/Scripts/SignIn&Up.js
@@ -10,6 +10,39 @@
         $("#login-modal").modal("hide")
     });
 
+    $("#registerSubmit").on("click", function () {
+        let uname = $("#registerInputUsername").val();
+        let email = $("#registerInputEmail").val();
+        let pass = $("#registerInputPassword").val();
+        let pass2 = $("#registerInputPassword2").val();
 
+        SubmitRegister(uname, email, pass, pass2);
+    })
 
 });
+
+
+function SubmitRegister(uname, email, pass, pass2) {
+
+    $.ajax({
+
+        url: '/User/Register/',
+        dataType: 'json',
+        data: {
+            username: uname,
+            email: email,
+            password: pass,
+            password2: pass2
+        },
+        success: function (data) {
+            var errors = data.errorList;
+
+
+            
+        },
+        error: function (jqXHR, statusText, errorThrown) {
+            $('#coinflipGameList').html('Ett fel inträffade: <br>'
+                + statusText);
+        }
+    });
+}
