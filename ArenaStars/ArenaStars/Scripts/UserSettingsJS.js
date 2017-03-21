6842a22431b0fbfb8a8fc435d25f676957777777
MuppetNicke <niclas-67@hotmail.com>
2017-03-21 21:04:09 +0100

    User settings 'done'.

---
+++ ArenaStars/ArenaStars/Scripts/UserSettingsJS.js
@@ -0,0 +1,56 @@
+﻿var updater = null;
+
+$(document).ready(function () {
+
+    SetSelectedOption();
+
+    $("#inputProfilePic").on("keyup", function () {
+
+        if (updater != 'null') {
+            clearTimeout(updater);
+        }
+
+        var updater = setTimeout(function () {
+
+            UpdateProfilePictures();
+
+        }, 3000);
+
+    });
+
+    $("#inputBackgroundPic").on("keyup", function () {
+
+        if (updater != 'null') {
+            clearTimeout(updater);
+        }
+
+        var updater = setTimeout(function () {
+
+            UpdateProfilePictures();
+
+        }, 3000);
+
+    });
+
+});
+
+
+
+function UpdateProfilePictures() {
+
+    if (updater != 'null') {
+        clearTimeout(updater);
+    }
+    
+    let profilePictureInput = $("#inputProfilePic").val();
+    let profileBackgroundInput = $("#inputBackgroundPic").val();
+
+    $("#profilePic").attr("src", profilePictureInput);
+    $('#profileBackground').css('background-image', 'url(' + profileBackgroundInput + ')');
+
+};
+
+function SetSelectedOption() {
+    let countryValue = $("#hiddenCountryValue").html();
+    $('#countrySelectList option[value=' + countryValue + ']').attr('selected', 'selected');
+};
